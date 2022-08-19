823880ec77b709ba8d622307bf9f9e38fded9438
Sashwat Jain <sashwatsjain@gmail.com>
2022-08-19 20:57:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,197 @@
 import mysql.connector
 
-db = mysql.connector.connect(
+def start():
+    # print(a)
+    print("*" * 89)
+    for x in range(2):
+        print("*", " " * 85, "*")
+
+    print("*                              EMPLOYEE MANAGEMENT SYSTEM                               *")
+    print("*                       Sashwat Jain, Bhavya Bajaj and Dhvij Shah                       *")
+    for x in range(2):
+        print("*", " " * 85, "*")
+    print("*" * 89)
+
+
+usr = input("SQL username : ")
+pwd = input("SQL password : ")
+
+mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="0000000000"
+    user=usr,
+    password=pwd
 )
 
-c = db.cursor()
-c.execute(f"create database if not exists ems;")
-c.execute(f"use ems;")
-c.execute("create table if not exists employees(id int, name varchar(30), salary int(50), position varchar(30), phoneNumber varchar(14), department varchar(30) primary key (id, phoneNumber) );")
-db.commit()
+print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("create database if not exists main;")
+mycursor.execute("use main;")
+mycursor.execute(
+    "create table if not exists employees(id int(8), name char(20), salary int, position varchar(10), phoneNumber varc"
+    "har(20), startyear varchar(11) NOT NULL,department VARCHAR(30) NOT NULL, primary key (id, phoneNumber) );")
 
 
-def addEmployes(cc):
-    id = int(input("id(int) : "))
-    name = str(input("name : "))
-    salary = int(input("salary(int) : "))
-    position = str(input("position : "))
-    phoneNumber = str(input("phone number : "))
-    department = str(input("Department : "))
+
+def new_staff(m):
+    a = int(input("id(int) : "))
+    b = str(input("name : "))
+    c = int(input("salary(int) : "))
+    d = str(input("position : "))
+    e = str(input("phone number : "))
+    f = str(input("start year : "))
+    g = str(input("department : "))
 
     try:
-        cc.execute(f"insert into employees values ({id}, '{name}', {salary}, '{position}', {phoneNumber});")
-        myresult = cc.fetchall()
-        for x in myresult:
-            print(x)
-
+        m.execute(f"insert into employees values ({a}, '{b}', {c}, '{d}', {e} , '{f}' , '{g}' );")
+        mydb.commit()
     except mysql.connector.errors.IntegrityError:
-        print("integrity error")
+        print("integrity error - employee not registered")
 
+def edit_details(m):
 
-def tableprint(cc):
-    cc.execute(f"select * from employees;")
-    myresult = cc.fetchall()
-    print("[id|      name      |   salary   | position |  phoneNumber  ]")
-    for x in myresult:
-        print(str(list(x)).replace(",", " |", 4))
-
-
-def find_staff(cc):
-    name = str(input("name : "))
-    cc.execute(f"select * from employees where name = '{name}';")
-    myresult = cc.fetchall()
-    for x in myresult:
-        print(f"\nid :{x[0]}\nname : {x[1]}\nsalary : {x[2]}\nposition : {x[3]}\nphoneNumber : {x[4]}")
-
-
-def remove_staff(c,d):
-    tableprint(c)
     a = int(input("id(int) : "))
-    c.execute(f"delete from employees where id = {a}")
-    d.commit()
+    m.execute(f"select * from employees where id = {a}")
+    myresult = m.fetchall()
+    if myresult:
+        for x in myresult:
+            print(f"\nid :{x[0]}\nname : {x[1]}\nsalary : {x[2]}\nposition : {x[3]}\nphoneNumber : {x[4]}\nstartyear : {x[5]}"
+                f"\ndepartment : {x[6]}")
+
+        b = str(input("name : "))
+        c = int(input("salary(int) : "))
+        d = str(input("position : "))
+        e = str(input("phone number : "))
+        f = str(input("start year : "))
+        g = str(input("department : "))
+
+        m.execute(f"update employees set name='{b}', salary={c}, position='{d}', phoneNumber={e}, startYear='{f}', department='{g}' where id={a};")
+        mydb.commit()
+        print("Employee Details Updated")
+    else:
+        print("Employee not found")
 
 
-def start():
-    print("*"*89)
-    for x in range(5):
-        print("*"," "*85,"*")
-    
-    print(f"*{' '*30}EMPLOYEE MANAGEMENT SYSTEM{' '*31}*")
+def full_table(m):
+    m.execute(f"select * from employees;")
+    myresult = m.fetchall()
+    tbl = "[   id |       name       |      salary     |  position  |phoneNumber |  startYear |   department  ]"
+    print("\n \nDisplaying Employee Menu...")
+    print(tbl)
+    for x in myresult:
+        print("%6s" % str(x[0])[0:4], "|", "%16s" % x[1][0:13], "|", "%15s" % str(x[2])[0:11], "|", "%10s" % x[3],
+              "|", "%10s" % x[4], "|", "%10s" % x[5], "|", "%10s" % x[6])
 
-    for x in range(5):
-        print("*"*89)
 
+def filteredtable(m):
+    print("""
+* * * * * * * * * * * * * * * 
+*  1. full table            *
+*  2. filter by salary      *
+*  3. filter by start year  *
+*  4. filter by department  *
+* * * * * * * * * * * * * * * 
+""")
+    o = int(input(">>>"))
+    if o == 1:
+        m.execute(f"select * from employees;")
+        myresult = m.fetchall()
+    elif o == 2:
+        j = str(input("Greater than, Less than or Equal [>,<,=] : "))
+        i = int(input("Enter Salary : "))
+        m.execute(f"select * from employees where Salary {j} {i};")
+        myresult = m.fetchall()
+    elif o == 3:
+        j = str(input("Greater than, Less than or Equal [>,<,=] : "))
+        i = str(input("Enter StartYear <YYYY>: "))
+        m.execute(f"select * from employees where StartYear {j} '{i}';")
+        myresult = m.fetchall()
+    elif o == 4:
+        i = str(input("Enter Department : "))
+        m.execute(f"select * from employees where Department='{i}';")
+        myresult = m.fetchall()
+    else:
+        print("invalid option")
+        pass
+
+    tbl = "[   id |       name      |      salary     |  position  |phoneNumber |  startYear |   department  ]"
+    print("\n \nDisplaying Employee Menu...")
+    print(tbl)
+    try:
+        for x in myresult:
+            print("%6s" % str(x[0])[0:4], "|", "%15s" % x[1][0:12], "|", "%15s" % str(x[2])[0:11], "|", "%10s" % x[3],
+                  "|", "%10s" % x[4], "|", "%10s" % x[5], "|", "%10s" % x[6])
+    except UnboundLocalError:
+        pass
+
+
+def find_staff(m):
+    opt = int(input("Search by : \n1.Name \n2.ID \nEnter Choice :"))
+
+    if opt == 2:
+        a = int(input("enter ID : "))
+        m.execute(f"select * from employees where ID= {a};")
+        myresult = m.fetchall()
+    elif opt == 1:
+        a = str(input("name : "))
+        m.execute(f"select * from employees where name = '{a}';")
+        myresult = m.fetchall()
+    print("\n \nSearching employee database...")
+
+    for x in myresult:
+        print(
+            f"\nid :{x[0]}\nname : {x[1]}\nsalary : {x[2]}\nposition : {x[3]}\nphoneNumber : {x[4]}\nstartyear : {x[5]}"
+            f"\ndepartment : {x[6]}")
+
+
+def employee_exists(m, a):
+    m.execute(f"select * from employees where id = {a}")
+    myresult = m.fetchall()
+    if myresult:
+        return True
+    else:
+        return False
+
+def remove_staff(m):
+    a = int(input("id(int) : "))
+    if employee_exists(m, a):
+        m.execute(f"select * from employees where id = {a}")
+        myresult = m.fetchall()
+        for x in myresult:
+            print(f"\nid :{x[0]}\nname : {x[1]}\nsalary : {x[2]}\nposition : {x[3]}\nphoneNumber : {x[4]}\nstartyear : {x[5]}"
+                f"\ndepartment : {x[6]}")
+        b = str(input("Are you sure you want to remove this employee? (y/n) : "))
+        if b == "y":
+            m.execute(f"delete from employees where id = {a}")
+            mydb.commit()
+            print("Employee FIRED")
+        else:
+            print("Employee not removed")
+    else:
+        print("Employee not found")
+start()
 
 while True:
-    print("1: add new employee")
-    print("2: list of employees")
-    print("3: find employee")
-    print("4: remove employee")
-    print("5: exit")
-    p = int(input(">>>"))
+    p = int(input("\n1 : List Of employees\n2 : Register new employee\n3 : Find Employee\n4 : FIRE Employee\n"
+                  "5 : Update Details\n6 : Exit\n>>>"))
     if p == 1:
-        addEmployes(c)
+        filteredtable(mycursor)
     elif p == 2:
-        tableprint(c)
+        new_staff(mycursor)
     elif p == 3:
-        find_staff(c)
+        find_staff(mycursor)
     elif p == 4:
-        remove_staff(c,db)
+        remove_staff(mycursor)
+        full_table(mycursor)
     elif p == 5:
-        db.commit()
-        db.close()
+        edit_details(mycursor)
+    elif p == 6:
+        print("committing changes")
+        mydb.commit()
+        print("terminating...")
+        mydb.close()
+        print("terminated")
         break
     else:
-        print("wrong input")
-        continue
+        print("invalid option selected")
 
-print("Thank you for using EmployeeManagementSystem", "created by :", " - Sashwat Jain", " - Bhavya Bajaj", " - Dhvij Shah", sep="\n")
